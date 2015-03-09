272fea2afe8d8c541e4e0dfd7a0562180dd59db3
Arnaud <arnaud.lods@gmail.com>
2015-03-09 08:18:55 +0100

    idvar idconst ident : première version

--- Compilateur/src/utility/Ident.java
+++ Compilateur/src/utility/Ident.java
@@ -1,5 +1,21 @@
 package utility;
 
 public abstract class Ident {
-
+	private String name;
+	private int type;
+	
+	public boolean memeIdent(Ident _a, Ident _b) {
+		
+		return _a.getName().equals(_b.getName());
+	}
+	
+	public Ident (String _name, int _type) {
+		this.name = _name;
+		this.type = _type;
+	}
+	
+	public String getName() { return this.name; }
+	public void setName(String _n) { this.name = _n; }
+	public int getType() { return this.type; }
+	public void setType(int _t) { this.type = _t; }
 }
