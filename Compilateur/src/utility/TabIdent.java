8d6610f92519ca744eb9f16b26640ed77e9370f1
William <wdazi@insa-rennes.fr>
2015-03-13 17:45:34 +0100

    Complétion de l'interface YVM pour corriger erreur compilation de la classe Interface.

--- Compilateur/src/utility/TabIdent.java
+++ Compilateur/src/utility/TabIdent.java
@@ -3,32 +3,29 @@ package utility;
 import java.util.HashMap;
 
 public class TabIdent {
-	private HashMap<String,Ident> table;
-	
-	public TabIdent () {
-		table = new HashMap<String,Ident>();
+	private HashMap<String, Ident> table;
+
+	public TabIdent() {
+		table = new HashMap<String, Ident>();
 	}
-	
+
 	public Ident chercheIdent(String _clef) throws IdentPasPresentException {
 		if (existeIdent(_clef)) {
 			return this.table.get(_clef);
-		}
-		else
+		} else
 			throw new IdentPasPresentException();
-		
+
 	}
-	
+
 	public boolean existeIdent(String _clef) {
 		return this.table.containsKey(_clef);
 	}
-	
-	public void rangeIdent (String _clef, Ident _ident) throws IdentDejaPresentException
-	{
-		if (!existeIdent(_clef))
-		{
+
+	public void rangeIdent(String _clef, Ident _ident)
+			throws IdentDejaPresentException {
+		if (!existeIdent(_clef)) {
 			this.table.put(_clef, _ident);
-		}
-		else
+		} else
 			throw new IdentDejaPresentException();
 	}
 }
--- Compilateur/src/utility/YVM.java
+++ Compilateur/src/utility/YVM.java
@@ -2,4 +2,28 @@ package utility;
 
 public interface YVM {
 
+	public void entete();
+
+	public void ouvrePrinc(int x);
+
+	public void iconst(int valeur);
+
+	public void idiv();
+
+	public void iadd();
+
+	public void istore(int offset);
+
+	public void iload(int x);
+
+	public void imul();
+
+	public void isub();
+
+	public void ior();
+
+	public void iinfegal();
+
+	public void queue();
+
 }
