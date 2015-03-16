5573bfdc30813912e1bfeca78ca419a5f88554f6
yporret <cburton@hotmail.fr>
2015-03-16 09:27:30 +0100

    ajout d'une variation de defConst

--- Compilateur/src/utility/Declaration.java
+++ Compilateur/src/utility/Declaration.java
@@ -14,6 +14,13 @@ public class Declaration {
 		((IdConst) Yaka.tabIdent.chercheIdent(clef)).setType(type);
 		
 	}
+
+	public void defConst(String clef, String nom) throws IdentPasPresentException{
+		IdConst temp = (IdConst) Yaka.tabIdent.chercheIdent(nom);
+		((IdConst) Yaka.tabIdent.chercheIdent(clef)).setValeur(temp.getValeur());
+		((IdConst) Yaka.tabIdent.chercheIdent(clef)).setType(temp.getType());
+		
+	}
 	
 	public void declVar(String clef, int type) throws IdentDejaPresentException{
 		Yaka.tabIdent.rangeIdent(clef, new IdVar(clef,type,0));
